bb5825eaa89bd6672e5d5ce033ae902f0f9943f1
efe <adambldwn110@gmail.com>
2020-12-05 18:53:44 +0300

    cookie

--- 05.12.2020/node1/index.js
+++ 05.12.2020/node1/index.js
@@ -1,10 +1,38 @@
 const express = require("express");
-require("dotenv").config()
+require("dotenv").config();
+
+var cookieParser = require("cookie-parser");
 
 const port = process.env.port || 5001;
 const host = "localhost";
 const app = express();
 
-app.listen(port, () => {
-    console.log("I'm listening on http://%s:%s", host, port);
+//middleware
+
+app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log("Cookies: ", req.signedCookies);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Controller // db");
+  if (res.isAuth) {
+    console.log("Private data");
+  } else {
+    console.log("not auth");
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware: 3");
+  if (true) {
+    res.send("send");
+  }
+});
+
+app.get("/user", (req, res) => {});
+app.listen(port, host, () => {
+  console.log("I'm listening on http://%s:%s", host, port);
 });
